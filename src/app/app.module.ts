491b3fe6289c49f13597826f234f9b003ad0def0
megirrena <megirrena12@gmail.com>
2024-03-16 00:43:17 +0100

    commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { CounterComponent } from './counter/counter.component';
+import { InputComponent } from './input/input.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CounterComponent,
+    InputComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
